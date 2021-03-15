c1db287686cb2d764b52e3b10bfc176f0deef2ca
tkmik <tkach_nikita@icloud.com>
2021-03-15 17:37:17 +0300

    added project Task81 with supporting mvc

---
+++ MetanitAspNetCore/Task81/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Task81.Models;
+
+namespace Task81.Controllers
+{
+    public class HomeController : Controller
+    {
+        private MobileContext db;
+
+        public HomeController(MobileContext context)
+        {
+            db = context;
+        }
+       
+        public IActionResult Index()
+        {
+            return View(db.Phones.ToList());
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.PhoneId = id;
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return $"Thanks {order.User}";
+        }
+    }
+}
---
+++ MetanitAspNetCore/Task81/SampleData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Task81.Models;
+
+namespace Task81
+{
+    public static class SampleData
+    {
+        public static void Initialize(MobileContext context)
+        {
+            if (!context.Phones.Any())
+            {
+                context.Phones.AddRange(
+                    new Phone 
+                    {
+                        Name = "S20 FE",
+                        Company = "Samsung",
+                        Price = 400
+                    },
+                    new Phone 
+                    {
+                        Name = "IPhone 5s",
+                        Company = "Apple",
+                        Price = 870
+                    }
+                    );
+                context.SaveChanges();
+            }
+        }
+    }
+}
