c8fe345b33bc41a078713001d25fe5180aeed597
Nikita <60256147+tkmik@users.noreply.github.com>
2021-03-19 14:10:02 +0300

    added new repository Task11_1

---
+++ MetanitAspNetCore/Task11_1/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Task11_1.Models;
+
+namespace Task11_1.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [Route("Main")]
+        [Route("Index")]
+        public IActionResult Index()
+        {
+            return Content($"Home/Index");
+        }
+
+        [Route("Privacy")]
+        public IActionResult Privacy()
+        {
+            return Content($"About");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
