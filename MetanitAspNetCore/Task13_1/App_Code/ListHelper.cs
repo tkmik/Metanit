179e09526b17cea337221fe967af9cccf168f8a1
Nikita <60256147+tkmik@users.noreply.github.com>
2021-03-22 00:14:29 +0300

    added the project with several HTML-helpers

---
+++ MetanitAspNetCore/Task13_1/App_Code/ListHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Task13_1.App_Code
+{
+    public static class ListHelper
+    {
+        public static HtmlString CreateList(this IHtmlHelper html, string[] items)
+        {
+            //string result = "<ul>";
+            //foreach (var item in items)
+            //{
+            //    result = $"{result}<li>{item}</li>";
+            //}
+            //result = $"{result}</ul>";
+            //return new HtmlString(result);
+
+            TagBuilder ul = new TagBuilder("ul");
+            foreach (var item in items)
+            {
+                TagBuilder li = new TagBuilder("li");
+                li.InnerHtml.Append(item);
+                ul.InnerHtml.AppendHtml(li);
+            }
+            ul.Attributes.Add("class", "itemList");
+            var writer = new System.IO.StringWriter();
+            ul.WriteTo(writer, HtmlEncoder.Default);
+            return new HtmlString(writer.ToString());
+        }
+    }
+}
