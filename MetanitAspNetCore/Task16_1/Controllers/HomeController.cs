765617652d4bbcb72f8ff0b3f0a1b74f46d8f0b4
Nikita <60256147+tkmik@users.noreply.github.com>
2021-03-25 13:36:59 +0300

    added Task16_1 working with validation

---
+++ MetanitAspNetCore/Task16_1/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Task16_1.Models;
+
+namespace Task16_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Person person)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content($"{person.Name} - {person.Email}");
+            }
+            else
+            {
+                return View(person);
+            }
+        }
+
+        [AcceptVerbs("Get", "Post")]
+        public IActionResult CheckEmail(string email)
+        {
+            if (email  == "admin@mail.ru")
+            {
+                return Json(false);
+            }
+            return Json(true);
+        }
+        
+    }
+}
---
+++ MetanitAspNetCore/Task16_1/Models/Person.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Task16_1.Models
+{
+    public class Person
+    {
+        [Required(ErrorMessage = "Введите имя")]
+        [StringLength(50, MinimumLength = 5, ErrorMessage = "Длина имени должна быть от 5 до 50 символов")]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        [Remote(action:"CheckEmail", controller:"Home", ErrorMessage = "Email уже используется")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        public string PasswordConfirm { get; set; }
+
+        [Range(14, 100, ErrorMessage = "Недопустимый возраст")]
+        public int Age { get; set; }
+
+        //[CreditCard]
+        //[Phone]
+        //[Url]
+
+    }
+}
