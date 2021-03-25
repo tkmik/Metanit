035dff726ea8b4b8f5c9925d47dba9b835569daf
Nikita <60256147+tkmik@users.noreply.github.com>
2021-03-26 02:57:14 +0300

    changed some mistakes, added some attributes

--- MetanitAspNetCore/Task16_1/Models/Person.cs
+++ MetanitAspNetCore/Task16_1/Models/Person.cs
@@ -9,25 +9,35 @@ namespace Task16_1.Models
 {
     public class Person
     {
-        [Required(ErrorMessage = "Введите имя")]
-        [StringLength(50, MinimumLength = 5, ErrorMessage = "Длина имени должна быть от 5 до 50 символов")]
+        //[Required(ErrorMessage = "Введите имя")]
+        //[StringLength(50, MinimumLength = 5, ErrorMessage = "Длина имени должна быть от 5 до 50 символов")]
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Введите имя")]
+        [Display(Name = "Имя пользователя")]
         public string Name { get; set; }
 
-        [Required]
-        [EmailAddress]
-        [Remote(action:"CheckEmail", controller:"Home", ErrorMessage = "Email уже используется")]
+        [Required(ErrorMessage = "Не указан электронный адрес")]
+        [EmailAddress(ErrorMessage = "Неккоретный электронный адрес")]
+        [Display(Name = "Электронная почта")]
+        //[Remote(action:"CheckEmail", controller:"Home", ErrorMessage = "Email уже используется")]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Не указан пароль")]
+        [Display(Name = "Пароль")]
+        [ScaffoldColumn(false)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [Display(Name = "Повтор пароля")]
+        [ScaffoldColumn(false)]
+        [DataType(DataType.Password)]
         public string PasswordConfirm { get; set; }
 
+        [Required(ErrorMessage = "Не указан возраст")]
         [Range(14, 100, ErrorMessage = "Недопустимый возраст")]
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
 
         //[CreditCard]
