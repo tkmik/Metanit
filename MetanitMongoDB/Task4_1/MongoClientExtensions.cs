623f92e4427441102acf48aa2ee61b838ffd2406
tkmik <tkach_nikita@outlook.com>
2021-04-18 22:13:41 +0300

    added a new repository to interact with MongoDB

---
+++ MetanitMongoDB/Task4_1/MongoClientExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System.IO;
+
+namespace Task4_1
+{
+    public static class MongoClientExtensions
+    {
+        public static string UseDefaultConnectionString(this MongoClient client)
+        {
+            var builder = new ConfigurationBuilder();
+            builder.SetBasePath(Directory.GetCurrentDirectory());
+            builder.AddJsonFile("appsettings.json");
+            var config = builder.Build();
+            var connectionString = config.GetConnectionString("Default");
+            return connectionString;
+        }
+    }
+}
---
+++ MetanitMongoDB/Task4_1/Program.cs
@@ -0,0 +1,70 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Threading.Tasks;
+
+namespace Task4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MongoClient client = new MongoClient();
+            client.UseDefaultConnectionString(); //my extension - connect to mongoDB
+            //GetDatabaseNames(client).GetAwaiter();
+            GetCollectionsNames(client).GetAwaiter();
+            BsonDocument doc = new BsonDocument()
+            {
+                { "Name", "Mikita" },
+                { "Surname", "Tkach" },
+                { "Age", new BsonInt32(25) },
+                { "Company",
+                    new BsonDocument
+                    {
+                        { "Name", "None" },
+                        { "Year", new BsonInt32(2021) }
+                    }
+                },
+                { "Countries", new BsonArray(new[] { "Belarus", "Russia" })}
+            };
+            Console.WriteLine(doc);
+
+            Console.ReadLine();
+        }
+
+        private static async Task GetDatabaseNames(MongoClient client)
+        {
+            using (var cursor = await client.ListDatabasesAsync())
+            {
+                var databaseDocuments = await cursor.ToListAsync();
+                foreach (var databaseDocument in databaseDocuments)
+                {
+                    Console.WriteLine(databaseDocument["name"]);
+                }
+                Console.WriteLine();
+            }
+        }
+        private static async Task GetCollectionsNames(MongoClient client)
+        {
+            using (var cursor = await client.ListDatabasesAsync())
+            {
+                var dbs = await cursor.ToListAsync();
+                foreach (var db in dbs)
+                {
+                    Console.WriteLine($"{db["name"]} contains the following collections:");
+
+                    IMongoDatabase database = client.GetDatabase(db["name"].ToString());
+                    using (var collCursor = await database.ListCollectionsAsync())
+                    {
+                        var colls = await collCursor.ToListAsync();
+                        foreach (var col in colls)
+                        {
+                            Console.WriteLine($"\t{col["name"]}");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
