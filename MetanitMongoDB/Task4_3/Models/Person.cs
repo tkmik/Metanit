b276fb6ddbd8d321760b34fc4db0ac12ab0ec82e
tkmik <tkach_nikita@outlook.com>
2021-04-19 16:03:26 +0300

    added a new project working with insertion into the database

---
+++ MetanitMongoDB/Task4_3/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4_1.Models
+{
+    class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        [BsonIgnoreIfDefault]
+        public int Age { get; set; }
+        [BsonIgnoreIfNull]
+        public Company Company { get; set; }
+        public List<string> Languages { get; set; }
+    }
+}
---
+++ MetanitMongoDB/Task4_3/Program.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
+using MongoDB.Driver;
+using System;
+using Task4_1.Models;
+
+namespace Task4_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person { Name = "person1", Surname = "Tkach", Age = 25 };
+            person1.Company = new Company { Name = "None" };
+
+            BsonDocument document = new BsonDocument()
+            {
+                { "Name", "person2" },
+                { "Surname", "Tkach" },
+                { "Age", new BsonInt32(25) },
+                { "Company", 
+                    new BsonDocument() {                    
+                        { "Name", "None" }
+                    } 
+                }
+            };
+            Person person2 = BsonSerializer.Deserialize<Person>(document);
+            BsonDocument person3 = person1.ToBsonDocument();
+
+            Console.WriteLine(person1.ToJson());
+            Console.WriteLine(person2.ToJson());
+
+
+            //BsonClassMap.RegisterClassMap<Person>(map => 
+            //{
+            //    map.AutoMap();
+            //    map.MapMember(p => p.Name).SetElementName("name");
+            //});
+
+            var client = new MongoClient();
+            client.UseDefaultConnectionString();
+            var database = client.GetDatabase("test");
+            var collection = database.GetCollection<BsonDocument>("people");
+
+            collection.InsertOne(document);
+            //collection.InsertMany(new[] { document, person3 });
+            var collection2 = database.GetCollection<Person>("people");
+            collection2.InsertOne(person1);
+            Console.ReadLine();
+        }
+    }
+}
